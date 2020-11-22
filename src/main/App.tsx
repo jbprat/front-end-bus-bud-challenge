88d19fbb50e1bcd3049d28ff1a04eb4f13974c41
Jean-Baptiste PE <jbprat-espouey@protonmail.com>
2020-11-22 12:01:05 -0500

    Add logo

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import osheagaLogo from './osheaga-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={osheagaLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
